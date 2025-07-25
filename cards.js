0c35f56da738014d6e926ce29e7149d61d93e86c
johnnydirtydog <120369606+johnnydirtydog@users.noreply.github.com>
2025-07-25 00:58:30 -0400

    Add files via upload

---
+++ cards.js
@@ -0,0 +1,7 @@
+
+const cards = [
+    { name: "The Fool", meaning: "New beginnings, adventure, spontaneity" },
+    { name: "The Magician", meaning: "Manifestation, resourcefulness, power" },
+    { name: "The High Priestess", meaning: "Intuition, mystery, divine feminine" },
+    // Add remaining 75 cards...
+];
