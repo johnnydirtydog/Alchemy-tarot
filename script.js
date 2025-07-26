8d654d1f2f1f6a735543fb20e3d17d59d38491f7
johnnydirtydog <120369606+johnnydirtydog@users.noreply.github.com>
2025-07-26 15:46:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,6 @@ async function drawCard() {
     card.classList.add('flipped');
   }, 1000);
 }
+document.getElementById('card-text').innerText = `${data.card.name}: ${data.card.meaning}`;
+const imagePath = `img/${data.card.name}.png`;
+document.getElementById('card-img').src = imagePath;
